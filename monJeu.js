c3c2977e7278b454a6e4253894d7177046257828
Sheolis <59622118+Sheolis@users.noreply.github.com>
2020-02-05 15:06:37 +0100

    hit box player corrigé

--- monJeu.js
+++ monJeu.js
@@ -63,7 +63,7 @@ function create(){
 	this.add.image(50,250,'surbloc1');
 
 
-	player = this.physics.add.sprite(100,450,'perso');
+	player = this.physics.add.sprite(70,80,'perso').setSize(30,67).setOffset(20,5);
 	player.setCollideWorldBounds(true);
 	player.setBounce(0.2);
 	player.body.setGravityY(000);
@@ -72,7 +72,7 @@ function create(){
 	cursors = this.input.keyboard.createCursorKeys();
 
 	this.anims.create({
-		key:'left',
+		key:'right',
 		frames: this.anims.generateFrameNumbers('perso', {start: 0, end: 3}),
 		frameRate: 10,
 		repeat: -1
@@ -80,8 +80,15 @@ function create(){
 
 	this.anims.create({
 		key:'stop',
-		frames: [{key: 'perso', frame:4}],
+		frames: [{key: 'perso', frame:0}],
 		frameRate: 20
+	});
+
+	this.anims.create({
+		key:'jump',
+		frames: this.anims.generateFrameNumbers('perso', {start: 4, end: 5}),
+		frameRate: 1,
+		repeat: 1
 	});
 
 	stars = this.physics.add.group({
@@ -102,20 +109,30 @@ function create(){
 
 
 function update(){
-	if(cursors.left.isDown){
-		player.anims.play('left', true);
+
+	if(cursors.left.isDown && player.body.touching.down){
+		player.anims.play('right', true);
 		player.setVelocityX(-300);
-		player.setFlipX(false);
-	}else if(cursors.right.isDown){
-		player.setVelocityX(300);
-		player.anims.play('left', true);
 		player.setFlipX(true);
+	}else if(cursors.right.isDown && player.body.touching.down){
+		player.setVelocityX(300);
+		player.anims.play('right', true);
+		player.setFlipX(false);
 	}else{
 		player.anims.play('stop', true);
 		player.setVelocityX(0);
 	}
 
+	if(cursors.left.isDown){
+		player.setVelocityX(-300);
+		player.setFlipX(true);
+	}else if(cursors.right.isDown){
+		player.setVelocityX(300);
+		player.setFlipX(false);
+	}
+
 	if(cursors.up.isDown && player.body.touching.down){
+		player.anims.play('jump', true);
 		player.setVelocityY(-330);
 	}
 
@@ -123,7 +140,6 @@ function update(){
 function hitBomb(player, bomb){
 	this.physics.pause();
 	player.setTint(0xff0000);
-	player.anims.play('turn');
 	gameOver=true;
 }
 
